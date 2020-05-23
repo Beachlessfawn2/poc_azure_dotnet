9340089f96c32a562737d6886a75375e0c6d9c5c
Beachlessfawn2 <kenzie.coddens.y1678@student.hogent.be>
2020-05-23 12:41:37 +0200

    [Code] Trying to fix error on script

--- ziping.ps1
+++ ziping.ps1
@@ -1,6 +1,6 @@
 $compress = @{
-    Path= "$(System.DefaultWorkingDirectory)/MessageUtil/bin/Release/netcoreapp3.1/*"
+    Path= "$(System.DefaultWorkingDirectory)\MessageUtil\bin\Release\netcoreapp3.1\*"
     CompressionLevel = "Fastest"
-    DestinationPath = "$(System.DefaultWorkingDirectory)/MessageUtil.zip"
+    DestinationPath = "$(System.DefaultWorkingDirectory)\MessageUtil.zip"
     }
 Compress-Archive @compress
