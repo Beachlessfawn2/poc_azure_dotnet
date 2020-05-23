757d4c7643b9e61a543de8b77489b1f3e61e2851
Beachlessfawn2 <kenzie.coddens.y1678@student.hogent.be>
2020-05-23 14:27:47 +0200

    [code] Trying to get standalone app

--- ziping.ps1
+++ ziping.ps1
@@ -1,5 +1,5 @@
 $compress = @{
-    Path= "d:\a\1\s\MessageUtil\bin\Release\netcoreapp3.1\publish\*"
+    Path= "d:\a\1\s\MessageUtil\bin\Release\netcoreapp3.1\win10-x64\publish\*"
     CompressionLevel = "Fastest"
     DestinationPath = "d:\a\1\s\MessageUtil.zip"
     }
